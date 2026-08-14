8970b790d56cad063bb4bf19f944066924e0f804
Елизавета <elizavetakolcova@gmail.com>
2026-08-14 14:04:26 +0300

    Create 08_товары_в_продаже.sql

---
+++ scripts/08_товары_в_продаже.sql
@@ -0,0 +1,21 @@
+CREATE TABLE "Товары_в_продаже"
+(
+    "Код_продажи"       INT NOT NULL,
+    "Код_товара"        INT NOT NULL,
+    "Количество"        INT NOT NULL,
+
+    CONSTRAINT "PK_Товары_в_продаже"
+        PRIMARY KEY ("Код_продажи", "Код_товара"),
+
+    CONSTRAINT "CHK_Товары_в_продаже_Количество"
+        CHECK ("Количество" > 0),
+
+    CONSTRAINT "FK_Товары_в_продаже_Продажи"
+        FOREIGN KEY ("Код_продажи")
+        REFERENCES "Продажи"("Код_продажи")
+        ON DELETE CASCADE,
+
+    CONSTRAINT "FK_Товары_в_продаже_Товар"
+        FOREIGN KEY ("Код_товара")
+        REFERENCES "Товар"("Код_товара")
+);
